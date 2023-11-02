9b90d84267d2e19bec6c31daabbe07a60ffd09b1
Yao Bin (DawnPhoenixYao) <854222741@qq.com>
2023-11-03 00:39:04 +1100

    StartScene Implement

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -14,5 +15,9 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
     }
 }
